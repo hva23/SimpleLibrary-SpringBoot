56502e3e6b413188403baa509fb52d8cf728dcb1
hva23 <haskihossein@gmail.com>
2023-10-22 00:34:12 +0330

    delete unnecessary import

--- SimpleLibrary/src/main/java/com/ister/config/DatabaseConfig.java
+++ SimpleLibrary/src/main/java/com/ister/config/DatabaseConfig.java
@@ -1,13 +1,10 @@
 package com.ister.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
-import java.sql.DriverManager;
-
 
 @Configuration
 public class DatabaseConfig {
