49f4a3e874b5484da7a82f8d8adbcfc5d89e0f3f
hva23 <haskihossein@gmail.com>
2023-10-22 00:52:39 +0330

    Update AuthorsFilter.java

--- SimpleLibrary/src/main/java/com/ister/filters/AuthorsFilter.java
+++ SimpleLibrary/src/main/java/com/ister/filters/AuthorsFilter.java
@@ -1,23 +1,19 @@
 package com.ister.filters;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
 import jakarta.servlet.*;
+
 import java.io.IOException;
 
 public class AuthorsFilter implements Filter {
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {
-
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        System.out.println("Authors Filter");
+        chain.doFilter(request, response);
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        System.out.println("In filter");
-        chain.doFilter(request, response);
+    public void init(FilterConfig filterConfig) {
+
     }
 
     @Override
