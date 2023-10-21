f98bd63cc3d1f830f6acf10a82246ce135c6161d
hva23 <haskihossein@gmail.com>
2023-10-21 10:50:33 +0330

    Create FilterConfig.java

---
+++ SimpleLibrary/src/main/java/com/ister/config/FilterConfig.java
@@ -0,0 +1,24 @@
+package com.ister.config;
+
+
+import com.ister.filters.AuthorsFilter;
+import com.ister.model.Author;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<AuthorsFilter> registerFilter() {
+        FilterRegistrationBean<AuthorsFilter> filterRegistrationBean = new FilterRegistrationBean();
+        AuthorsFilter authorsFilter = new AuthorsFilter();
+
+        filterRegistrationBean.setFilter(authorsFilter);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+}
