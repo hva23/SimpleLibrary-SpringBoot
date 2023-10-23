dd4dcb72cbb14311dcef6cfc6f88df038779b965
hva23 <haskihossein@gmail.com>
2023-10-23 13:52:08 +0330

    add inMemoryAuth

--- SimpleLibrary/src/main/java/com/ister/config/SecurityConfig.java
+++ SimpleLibrary/src/main/java/com/ister/config/SecurityConfig.java
@@ -3,13 +3,17 @@ package com.ister.config;
 
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.http.HttpFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,5 +31,20 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable);  //Disable CSRF because of using postman to make requests
         return http.build();
+    }
+
+
+    //You can add some authenticated user here
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
