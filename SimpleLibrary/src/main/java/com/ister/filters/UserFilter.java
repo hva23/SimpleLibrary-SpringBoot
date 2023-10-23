494447a221a7cc28254bdd58458b44e012221826
hva23 <haskihossein@gmail.com>
2023-10-23 17:32:10 +0330

    rename AuthorFilter -> UserFilter

--- SimpleLibrary/src/main/java/com/ister/filters/UserFilter.java
+++ SimpleLibrary/src/main/java/com/ister/filters/UserFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.*;
 
 import java.io.IOException;
 
-public class AuthorsFilter implements Filter {
+public class UserFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         System.out.println("Authors Filter");
