32f4b52ae53d0856a8a00002f880f0dea35399ae
hva23 <haskihossein@gmail.com>
2023-10-23 15:32:57 +0330

    rename AuthorRowMapper -> UserRowMapper

--- SimpleLibrary/src/main/java/com/ister/mappers/UserRowMapper.java
+++ SimpleLibrary/src/main/java/com/ister/mappers/UserRowMapper.java
@@ -1,18 +1,19 @@
 package com.ister.mappers;
 
-import com.ister.model.Author;
+import com.ister.model.User;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AuthorRowMapper implements RowMapper<Author> {
+public class UserRowMapper implements RowMapper<User> {
     @Override
-    public Author mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Author author = new Author();
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User author = new User();
         author.setId(rs.getString("ID"));
         author.setName(rs.getString("NAME"));
         author.setPassword(rs.getString("PASSWORD"));
+        author.setRole(rs.getString("ROLE"));
         return author;
     }
 }
