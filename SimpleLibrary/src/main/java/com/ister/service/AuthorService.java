2e1484697c993b145a85020b269ad4427f8e2228
hva23 <haskihossein@gmail.com>
2023-10-23 15:34:12 +0330

    add role validation

--- SimpleLibrary/src/main/java/com/ister/service/AuthorService.java
+++ SimpleLibrary/src/main/java/com/ister/service/AuthorService.java
@@ -1,36 +1,49 @@
 package com.ister.service;
 
-import com.ister.model.Author;
-import com.ister.repository.AuthorRepo;
-import com.ister.repository.BaseRepo;
+import com.ister.common.Roles;
+import com.ister.model.User;
+import com.ister.repository.UserRepo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AuthorService {
-    private final AuthorRepo repository;
+    private final UserRepo repository;
 
-    public AuthorService(AuthorRepo repo) {
+    public AuthorService(UserRepo repo) {
         this.repository = repo;
     }
 
-    public boolean add(Author author) {
-        return repository.create(author);
+    public boolean add(User user) {
+        String role = user.getRole();
+        //If user role isn't any of allowed return false otherwise do the creation
+        if (!role.contentEquals(Roles.ADMIN.toString()))
+            if (!role.contentEquals(Roles.AUTHOR.toString()))
+                if (!role.contentEquals(Roles.USER.toString())) return false;
+
+        return repository.create(user);
     }
-    public boolean edit(Author author) {
-        return repository.update(author);
+
+    public boolean edit(User user) {
+        String role = user.getRole();
+        //If user role isn't any of allowed return false otherwise do the updating
+        if (!role.contentEquals(Roles.ADMIN.toString()))
+            if (!role.contentEquals(Roles.AUTHOR.toString()))
+                if (!role.contentEquals(Roles.USER.toString())) return false;
+
+        return repository.update(user);
     }
 
     public boolean delete(String id) {
         return repository.delete(id);
     }
 
-    public List<Author> getAll() {
+    public List<User> getAll() {
         return repository.findAll();
     }
 
-    public Author getById(String id) {
+    public User getById(String id) {
         return repository.findById(id);
     }
 }
