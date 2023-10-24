4bbf6e8056544f4684aad101476ac2bf058ca11e
hva23 <haskihossein@gmail.com>
2023-10-24 22:35:08 +0330

    should be add these to use JpaRepository

--- SimpleLibrary/src/main/java/com/ister/simplespringboot/SimpleSpringBootApplication.java
+++ SimpleLibrary/src/main/java/com/ister/simplespringboot/SimpleSpringBootApplication.java
@@ -4,18 +4,22 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@SpringBootApplication(scanBasePackages = "com.ister")
-@ServletComponentScan
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.ister.repository.jpa.hibernate") //Scanning Jpa Repositories
+@ComponentScan(basePackages = "com.ister")      //Scanning @Components, @Services, @Configurations
+@EntityScan(basePackages = "com.ister.model")   //Scanning @Entity
 public class SimpleSpringBootApplication {
 
     public static void main(String[] args) {
