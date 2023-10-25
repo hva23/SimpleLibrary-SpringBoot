0f55183fca655af2433c6042b88a3cbd94abe0dc
hva23 <haskihossein@gmail.com>
2023-10-25 13:16:58 +0330

    implement from UserDetails

--- SimpleLibrary/src/main/java/com/ister/model/User.java
+++ SimpleLibrary/src/main/java/com/ister/model/User.java
@@ -1,16 +1,60 @@
 package com.ister.model;
 
 
+import com.ister.common.Roles;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity<String> {
+public class User extends BaseEntity<String> implements UserDetails {
     private String password;
-    private String role;
+    @Enumerated(value = EnumType.STRING)
+    private Roles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new SimpleGrantedAuthority(role.toString()));
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
