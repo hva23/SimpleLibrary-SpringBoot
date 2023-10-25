3ec3189035e92a4981f5c2626cb16820a38fe2c8
hva23 <haskihossein@gmail.com>
2023-10-25 13:16:33 +0330

    Create UserDetailsServiceImpl.java

---
+++ SimpleLibrary/src/main/java/com/ister/config/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ister.config.security;
+
+import com.ister.model.User;
+import com.ister.repository.jpa.hibernate.UserJpaRepo;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserJpaRepo repository;
+
+    public UserDetailsServiceImpl(UserJpaRepo repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("Doing something else");
+        return repository.findByName(username).orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+    }
+}
